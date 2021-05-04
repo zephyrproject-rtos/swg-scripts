37e754f63bce3e980d7a9b4bebf99381c58683be
David Brown <david.brown@linaro.org>
2021-05-04 07:41:44 -0600

    GHSA HTML scraping attempt
    This attempts to extract the GHSA information from the fetched HTML.  It
    seems to currently be broken, which will need more investigation.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- ghsa/ghsa.py
+++ ghsa/ghsa.py
@@ -45,7 +45,7 @@ class App():
 
     def fetch_1_index(self, state, page):
         payload = {'page': str(page), 'state': state}
-        r = requests.get(self.__url(state, page), params=payload)
+        r = requests.get(self.__url(), params=payload)
 
         soup = BeautifulSoup(r.text, 'html.parser')
         e = soup.select('a')
@@ -73,7 +73,68 @@ class App():
             page += 1
         return result
 
-    def __url(self, state, page):
+    def fetch_ghsa(self, ghsa):
+        """
+        Attempt to fetch and decode a GHSA.  If we have an error or
+        permission problems, can return None.
+        """
+        url = self.__url() + '/' + ghsa
+        r = requests.get(url)
+        # print(r.text)
+
+        fields = {}
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        # GHSA number.  To verify we got a page.
+        # e = soup.select('div.js-details-container div.TableObject-item--primary span')
+        e = soup.select('div.TableObject-item--primary span')
+        assert len(e) == 1
+        e = e[0].contents
+        assert len(e) == 1
+        fields['ghsa'] = e[0]
+        assert fields['ghsa'] == ghsa
+
+        # Affects and fixed versions.
+        e = soup.select('div.f4')
+        assert len(e) == 2
+        fields['affects'] = e[0].contents
+        fields['fixed'] = e[1].contents
+
+        e = soup.select('h1.gh-header-title')
+        assert len(e) == 1
+        e = e[0].contents
+        assert len(e) == 1
+        fields['title'] = e[0].strip()
+
+        e = soup.select('div.pl-md-4 div.pt-2')
+        assert len(e) == 2
+        fields['cve'] = e[0].contents[0]
+        fields['cvss-pri'] = e[1].contents[0]
+
+        e = soup.select('div.pl-md-4 div.pt-1')
+        assert len(e) == 1
+        fields['cvss'] = e[0].contents[0]
+
+        # e = soup.select('textarea[name="repository_advisory[description]"]')
+        # assert len(e) == 1
+        # fields['description'] = e[0].contents
+
+        # BROKEN
+        e = soup.select('div.js-cwe-list span')
+        assert len(e) % 2 == 0
+        res = []
+        for i in range(0, len(e), 2):
+            desc = e[i].contents
+            assert len(desc) == 1
+            tag = e[i+1].contents
+            assert len(tag) == 1
+            res.append("{} {}".format(desc[0], tag[0]))
+        fields['cwe'] = res
+
+        return fields
+
+    def __url(self):
         return "https://github.com/zephyrproject-rtos/zephyr/security/advisories"
 
 
@@ -82,6 +143,8 @@ def main():
 
     adv = app.fetch_index(state='published')
     print(adv)
+    g0 = app.fetch_ghsa(adv[0])
+    print(g0)
 
 
 if __name__ == '__main__':
