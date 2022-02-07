a1d071f0c5cf2ee49e05ede8ac8d986c7c5f6af0
David Brown <david.brown@linaro.org>
2022-02-07 12:45:37 -0700

    Update RS scraper

--- ghsa/src/main.rs
+++ ghsa/src/main.rs
@@ -1,6 +1,11 @@
 use curl::easy::Easy;
-use anyhow::Result;
-use scraper::{Html, Selector};
+use anyhow::{anyhow, Result};
+use scraper::{
+    ElementRef,
+    Html,
+    html::Select,
+    Selector,
+};
 use std::{
     fs::File,
     io::Write,
@@ -19,7 +24,9 @@ fn main() -> Result<()> {
 
     let advs = Advisory::get_list(&mut easy)?;
     println!("Got {} advisories", advs.len());
-    println!("{:#?}", advs);
+    // println!("{:#?}", advs);
+
+    let () = advs[0].fetch(&mut easy)?;
 
     Ok(())
 }
@@ -40,17 +47,7 @@ impl Advisory {
 
         while let Some(url) = next_url {
             println!("Url: {}", url);
-            easy.url(&url)?;
-            let mut buf = Vec::new();
-            {
-                let mut transfer = easy.transfer();
-                transfer.write_function(|data| {
-                    buf.extend_from_slice(data);
-                    Ok(data.len())
-                })?;
-                transfer.perform()?;
-            }
-            let text = str::from_utf8(&buf)?;
+            let text = easy.fetch(&url)?;
 
             let doc = Html::parse_document(&text);
 
@@ -86,6 +83,59 @@ impl Advisory {
 
         Ok(result)
     }
+
+    /// Given an advisory fetch it and return the extracted data.
+    fn fetch(&self, easy: &mut Easy) -> Result<()> {
+        let text = easy.fetch(&format!("{}{}", GITHUB, self.url))?;
+        // dump("ghsa1.html", text.as_bytes())?;
+        let doc = Html::parse_document(&text);
+
+        // Get the GHSA name.
+        let sel = Selector::parse("div.TableObject-item--primary span").unwrap();
+        let elt = single(doc.select(&sel))?;
+        let name = text1(elt)?;
+        println!("name: {:?}", name);
+
+        // Affects and fixed versions.
+        let sel = Selector::parse("div.f4").unwrap();
+        let elts: Result<Vec<&str>> = doc.select(&sel).map(|elt| text1(elt)).collect();
+        let elts = elts?;
+
+        let affects = elts[0].to_owned();
+        let fixed = elts[1].to_owned();
+        println!("affects: {}", affects);
+        println!("fixed: {}", fixed);
+        Ok(())
+    }
+}
+
+/// Given an elt iterator, ensure that it has a single return value, otherwise return an error.
+fn single<'a, 'b>(mut elts: Select<'a, 'b>) -> Result<ElementRef<'a>> {
+    let result = if let Some(elt) = elts.next() {
+        elt
+    } else {
+        return Err(anyhow!("Selector returned no results"));
+    };
+    if elts.next().is_some() {
+        return Err(anyhow!("Selector returned too many results"));
+    }
+
+    Ok(result)
+}
+
+/// Get the single text from this element.  The result will be the trimmed string result.
+fn text1<'a>(elt: ElementRef<'a>) -> Result<&'a str> {
+    let mut iter = elt.text();
+    let result = if let Some(text) = iter.next() {
+        text.trim()
+    } else {
+        return Err(anyhow!("Selector returned node with no text"));
+    };
+    if let Some(_) = iter.next() {
+        return Err(anyhow!("Selector returned multiple text results"));
+    }
+
+    Ok(result)
 }
 
 /// Generate an advisory url for a given page.
@@ -99,4 +149,27 @@ fn advisory_url(page: usize) -> String {
 fn dump<P: AsRef<Path>>(p: P, data: &[u8]) -> Result<()> {
     File::create(p)?.write_all(data)?;
     Ok(())
+}
+
+/// Some extensions that make Easy even easier.
+trait EasyExt {
+    /// Retrieve the given URL (leaving everything else unchanged), fetching the results, and
+    /// returning them in a string.
+    fn fetch(&mut self, url: &str) -> Result<String>;
+}
+
+impl EasyExt for Easy {
+    fn fetch(&mut self, url: &str) -> Result<String> {
+        self.url(url)?;
+        let mut buf = Vec::new();
+        {
+            let mut transfer = self.transfer();
+            transfer.write_function(|data| {
+                buf.extend_from_slice(data);
+                Ok(data.len())
+            })?;
+            transfer.perform()?;
+        }
+        Ok(String::from_utf8(buf)?)
+    }
 }
